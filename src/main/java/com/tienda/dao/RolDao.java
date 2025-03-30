a22d9eb716d7a4b736f4f23c2beed756ec51309f
Villamil <josepablov98@gmail.com>
2025-03-30 15:55:10 -0600

    Semana11

--- src/main/java/com/tienda/dao/RolDao.java
+++ src/main/java/com/tienda/dao/RolDao.java
@@ -1,9 +1,10 @@
 package com.tienda.dao;
 
+import com.tienda.domain.Rol;
 import com.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+public interface RolDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
     
     Usuario findByUsernameAndPassword(String username, String Password);
@@ -11,4 +12,6 @@ public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsernameOrCorreo(String username, String correo);
 
     boolean existsByUsernameOrCorreo(String username, String correo);
+
+    public void save(Rol rol);
 }
