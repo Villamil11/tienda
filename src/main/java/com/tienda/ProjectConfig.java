4e17084c6d80bf41261294b2ba61c1ef22401f6c
Villamil <josepablov98@gmail.com>
2025-04-04 20:28:41 -0600

    Semana 12

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -107,37 +107,36 @@ public class ProjectConfig implements WebMvcConfigurer {
         build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((request) -> request
-                .requestMatchers("/", "/index", "/errores/**", "/error",
-                        "/carrito/**", "/pruebas/**", "/reportes/**",
-                        "/registro/**", "/js/**", "/webjars/**")
-                .permitAll()
-                .requestMatchers(
-                        "/producto/nuevo", "/producto/guardar",
-                        "/producto/modificar/**", "/producto/eliminar/**",
-                        "/categoria/nuevo", "/categoria/guardar",
-                        "/categoria/modificar/**", "/categoria/eliminar/**",
-                        "/usuario/nuevo", "/usuario/guardar",
-                        "/usuario/modificar/**", "/usuario/eliminar/**",
-                        "/reportes/**"
-                ).hasRole("ADMIN")
-                .requestMatchers(
-                        "/producto/listado",
-                        "/categoria/listado",
-                        "/usuario/listado"
-                ).hasRole("VENDEDOR")
-                .requestMatchers("/facturar/carrito")
-                .hasRole("USER")
-                )
-                .formLogin((form) -> form
-                .loginPage("/login").permitAll())
-                .logout((logout) -> logout.permitAll());
-                
-        return http.build();
-    }
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	http
+			.authorizeHttpRequests((request) -> request
+			.requestMatchers("/", "/index", "/errores/**",
+					"/carrito/**", "/reportes/**",
+					"/registro/**", "/js/**", "/webjars/**", "/error", "/refrescarBoton")
+			.permitAll()
+			.requestMatchers(
+					"/producto/nuevo", "/producto/guardar",
+					"/producto/modificar/**", "/producto/eliminar/**",
+					"/categoria/nuevo", "/categoria/guardar",
+					"/categoria/modificar/**", "/categoria/eliminar/**",
+					"/usuario/nuevo", "/usuario/guardar",
+					"/usuario/modificar/**", "/usuario/eliminar/**",
+					"/reportes/**", "/pruebas/**"
+			).hasRole("ADMIN")
+			.requestMatchers(
+					"/producto/listado",
+					"/categoria/listado",
+					"/usuario/listado"
+			).hasAnyRole("ADMIN", "VENDEDOR")
+			.requestMatchers("/facturar/carrito")
+			.hasRole("USER")
+			)
+			.formLogin((form) -> form
+			.loginPage("/login").permitAll())
+			.logout((logout) -> logout.permitAll());
+	return http.build();
+}
         }
 
    
